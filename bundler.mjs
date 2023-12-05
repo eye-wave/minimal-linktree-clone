b949285e7ccd0c021df63ef7c4458a9ce0b5f5f9
_eyewave <89079979+eye-wave@users.noreply.github.com>
2023-12-05 19:54:26 +0100

    fix date

--- bundler.mjs
+++ bundler.mjs
@@ -12,7 +12,7 @@ const mini = minify(
   html
     .replace(/<link.*css.*>/, "<style>" + cleanedCss.styles + "</style>")
     .replace(/<script.*js.*>/, "<script>" + js + "</script>")
-    .replace(/%age%/,Math.floor((Date.now() - 107784e7) / 315576e4))
+    .replace(/%age%/,Math.floor((Date.now() - 107784e7) / 315576e5))
   , {
   collapseWhitespace: true,
   minifyCSS: true,
